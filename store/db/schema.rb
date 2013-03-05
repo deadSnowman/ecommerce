5059a59adaa7a5cd92a212e5a0f8f98e670b6ab3
Seth Thomas <seth@mmph.(none)>
2013-03-05 18:48:47 -0500

    Added description columb to products

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223222450) do
+ActiveRecord::Schema.define(:version => 20130305224757) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.decimal  "price"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
   end
 
 end
