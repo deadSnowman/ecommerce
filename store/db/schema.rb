7b905c0ef1b9533c9df342b6d1a78eb72eef48f3
Seth Thomas <seth@mmph.(none)>
2013-03-10 04:09:02 -0400

    Added boolean instock to products table

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130305224757) do
+ActiveRecord::Schema.define(:version => 20130310074236) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130305224757) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.text     "description"
+    t.boolean  "instock"
   end
 
 end
