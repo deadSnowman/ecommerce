6d281c934b213548bb254b0338a23137afec78fd
Seth Thomas <seth@mmph.(none)>
2013-03-13 02:02:06 -0400

    Stage removes

--- store/app/models/product.rb
+++ store/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price, :description, :instock
+  attr_accessible :name, :price, :description, :instock, :category_id
+
+  belongs_to :categories, :foreign_key => 'category_id'
 end
--- store/db/schema.rb
+++ store/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130311005431) do
+ActiveRecord::Schema.define(:version => 20130311024541) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130311005431) do
     t.datetime "updated_at",  :null => false
     t.text     "description"
     t.boolean  "instock"
+    t.integer  "category_id"
   end
 
 end
